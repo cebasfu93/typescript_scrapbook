72e0c68f63d692727db4b73596d8f28be56b768e
Sebastian Franco Ulloa <sebastian.ulloa@xal.no>
2024-05-06 16:01:36 +0200

    finished 02tut

---
+++ 02tut/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = "Sebastian";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "John";
+meaningOfLife = 42;
+isLoading = true;
+album = true;
+const sum = (a, b) => {
+    return a + b;
+};
+let postID;
+let isActive;
+let re = /\w+/g;
---
+++ 02tut/src/main.ts
@@ -0,0 +1,17 @@
+let myName: string = "Sebastian";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "John";
+meaningOfLife = 42;
+isLoading = true;
+album = true;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+let postID: string | number;
+let isActive: number | boolean;
+let re: RegExp = /\w+/g;
