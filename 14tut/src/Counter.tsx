352fe2f0e0f39ef868c2c3695022de09b0838291
Sebastian Franco Ulloa <sebastian.ulloa@xal.no>
2024-05-10 10:04:16 +0200

    finished 15tut

--- 14tut/src/Counter.tsx
+++ 14tut/src/Counter.tsx
@@ -1,18 +1,22 @@
 import { ChangeEvent, ReactNode, useReducer } from "react";
 
+// state variable
 const initState = { count: 0, text: "" };
 
+// enum with a category for all possible actions
 const enum REDUCER_ACTION_TYPE {
   INCREMENT,
   DECREMENT,
   NEW_INPUT,
 }
 
+// typing for the action
 type ReducerAction = {
   type: REDUCER_ACTION_TYPE;
   payload?: string;
 };
 
+// function to handle all the possible cases triggers of the reducer
 const reducer = (state: typeof initState, action: ReducerAction): typeof initState => {
   switch (action.type) {
     case REDUCER_ACTION_TYPE.INCREMENT:
@@ -26,22 +30,23 @@ const reducer = (state: typeof initState, action: ReducerAction): typeof initSta
   }
 };
 
+// typing for the children of Counter
 type ChildrenType = {
   children: (num: number) => ReactNode;
 };
 
+// main component
 const Counter = ({ children }: ChildrenType) => {
-  // const [count, setCount] = useState<number>(1);
-  // const increment = () => setCount((prev) => prev + 1);
-  // const decrement = () => setCount((prev) => prev - 1);
-
+  // define reducer with a start varibale and the case handler (aka reducer)
   const [state, dispatch] = useReducer(reducer, initState);
+  // extract functions from the reducer
   const increment = () => dispatch({ type: REDUCER_ACTION_TYPE.INCREMENT });
   const decrement = () => dispatch({ type: REDUCER_ACTION_TYPE.DECREMENT });
   const handleTextInput = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch({ type: REDUCER_ACTION_TYPE.NEW_INPUT, payload: e.target.value });
   };
 
+  // build the component calling the reducer functions
   return (
     <>
       <h1>{children(state.count)}</h1>
@@ -54,5 +59,3 @@ const Counter = ({ children }: ChildrenType) => {
     </>
   );
 };
-
-export default Counter;
